5418727bf09dbe8bf81133d73f29e33c22786a04
AngeATT <96740251+AngeATT@users.noreply.github.com>
2023-03-27 20:28:08 +0000

    init

---
+++ src/main/java/tacos/authorization/authorization/TacoAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package tacos.authorization.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoAuthorizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tacos/authorization/authorization/TacoAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package tacos.authorization.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
